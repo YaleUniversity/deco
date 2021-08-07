1fa3c4c7a347c9ff3ad185fdab60d7b2d9850fa4
E Camden Fisher <fish@fishnix.net>
2021-08-07 09:07:42 -0400

    add support for generating keys, encrypting/decrypting values and dencrypted values in the control file

---
+++ cmd/encrypt.go
@@ -0,0 +1,128 @@
+package cmd
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/YaleUniversity/deco/control"
+	"github.com/spf13/cobra"
+)
+
+var (
+	encryptionKey string
+)
+
+func init() {
+	RootCmd.AddCommand(encryptionCmd)
+	encryptionCmd.PersistentFlags().StringVar(&encryptionKey, "key", "", "256bit encryption key")
+	encryptionCmd.AddCommand(encryptionGenKey)
+	encryptionCmd.AddCommand(encryptValue)
+	encryptionCmd.AddCommand(decryptValue)
+}
+
+// encryptionCmd represents the show command
+var encryptionCmd = &cobra.Command{
+	Use:   "encryption",
+	Short: "Manage encryption",
+	Long:  `Manage encryption mechanisms for deco values.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if value := os.Getenv("DECO_ENCRYPTION_KEY"); encryptionKey == "" && value != "" {
+			encryptionKey = value
+		}
+
+		return nil
+	},
+}
+
+var encryptionGenKey = &cobra.Command{
+	Use:   "genkey",
+	Short: "Generate a new encryption key",
+	Long:  `Generates a new random 256bit encrytion key.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		key, err := control.NewEncryptionKey()
+		if err != nil {
+			return fmt.Errorf("failed to generate key: %s", err)
+		}
+
+		fmt.Printf("%x\n", *key)
+
+		return nil
+	},
+}
+
+var encryptValue = &cobra.Command{
+	Use:   "encrypt [value]",
+	Short: "Encrypt a value",
+	Long:  `Generates cyphertext of the passed value using the encryption key found in the environment or passed via flags.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return fmt.Errorf("unexpected number of arguments: %d", len(args))
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		key, err := getKey()
+		if err != nil {
+			return err
+		}
+
+		cipherText, err := control.Encrypt([]byte(args[0]), key)
+		if err != nil {
+			return fmt.Errorf("failed to encrypt to ciphertext: %s", err)
+		}
+
+		fmt.Printf("%x\n", cipherText)
+
+		return nil
+	},
+}
+
+var decryptValue = &cobra.Command{
+	Use:   "decrypt [cipher]",
+	Short: "Decrypt a value",
+	Long:  `Decryptes passed ciphertext using the encryption key found in the environment or passed via flags.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		key, err := getKey()
+		if err != nil {
+			return err
+		}
+
+		cipherBytes, err := hex.DecodeString(args[0])
+		if err != nil {
+			return fmt.Errorf("invalid ciphertext encoding: %s", err)
+		}
+
+		plainText, err := control.Decrypt(cipherBytes, key)
+		if err != nil {
+			return fmt.Errorf("failed to decrypt ciphertext: %s", err)
+		}
+
+		fmt.Printf("%s\n", plainText)
+
+		return nil
+	},
+}
+
+func getKey() (*[32]byte, error) {
+	if encryptionKey == "" {
+		return nil, errors.New("missing encryption key")
+	}
+
+	keyBytes, err := hex.DecodeString(encryptionKey)
+	if err != nil {
+		return nil, fmt.Errorf("invalid key encoding: %s", err)
+	}
+
+	var key [32]byte
+	copy(key[:], keyBytes)
+
+	return &key, nil
+}
---
+++ control/encrypt.go
@@ -0,0 +1,70 @@
+package control
+
+// Provides symmetric authenticated encryption using 256-bit AES-GCM with a random nonce.
+// Copied and modified from https://github.com/gtank/cryptopasta
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"io"
+)
+
+// NewEncryptionKey generates a random 256-bit key for Encrypt() and
+// Decrypt(). It return an error if the source of randomness fails.
+func NewEncryptionKey() (*[32]byte, error) {
+	key := [32]byte{}
+	if _, err := io.ReadFull(rand.Reader, key[:]); err != nil {
+		return nil, err
+	}
+	return &key, nil
+}
+
+// Encrypt encrypts data using 256-bit AES-GCM.  This both hides the content of
+// the data and provides a check that it hasn't been altered. Output takes the
+// form nonce|ciphertext|tag where '|' indicates concatenation.
+func Encrypt(plaintext []byte, key *[32]byte) (ciphertext []byte, err error) {
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+// Decrypt decrypts data using 256-bit AES-GCM.  This both hides the content of
+// the data and provides a check that it hasn't been altered. Expects input
+// form nonce|ciphertext|tag where '|' indicates concatenation.
+func Decrypt(ciphertext []byte, key *[32]byte) (plaintext []byte, err error) {
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < gcm.NonceSize() {
+		return nil, errors.New("malformed ciphertext")
+	}
+
+	return gcm.Open(nil,
+		ciphertext[:gcm.NonceSize()],
+		ciphertext[gcm.NonceSize():],
+		nil,
+	)
+}
